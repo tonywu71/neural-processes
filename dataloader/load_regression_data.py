e56364dffd7dac41ea2a8ec55d5b734993635ce2
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2023-02-08 00:16:54 +0000

    Change RegressionDataGenerator - Done to ensure temporary legacy before merge

--- dataloader/load_regression_data.py
+++ dataloader/load_regression_data.py
@@ -10,12 +10,31 @@ import matplotlib.pyplot as plt
 
 tfd = tfp.distributions
 
-MIN_NUM_CONTEXT = 3
-MIN_NUM_TARGET = 2
-
-
-def gen(x_values, gp, batch_size, num_context, num_target, iterations, testing) -> Iterator[Tuple[Tuple[tf.Tensor, tf.Tensor, tf.Tensor], tf.Tensor]]:        
+def gen(batch_size, iterations, kernel_length_scale,
+        min_num_context, max_num_context, min_num_target,
+        testing) -> Iterator[Tuple[Tuple[tf.Tensor, tf.Tensor, tf.Tensor], tf.Tensor]]:        
+    kernel = tfp.math.psd_kernels.ExponentiatedQuadratic(length_scale=kernel_length_scale)
+    
     for _ in range(iterations):
+        num_context = tf.random.uniform(shape=[],
+                                    minval=min_num_context,
+                                    maxval=max_num_context,
+                                    dtype=tf.int32)
+
+        num_target = tf.random.uniform(shape=[],
+                                    minval=min_num_target,
+                                    maxval=max_num_context,
+                                    dtype=tf.int32)
+        
+        num_total_points = num_context + num_target
+        
+        x_values = tf.random.uniform(shape=(batch_size, num_total_points, 1),
+                                        minval=-2,
+                                        maxval=2)
+        
+        gp = tfd.GaussianProcess(kernel, index_points=x_values, jitter=1.0e-4)
+        y_values = tf.expand_dims(gp.sample(), axis=-1)
+        
         if testing is True:
             num_target = 400
             num_total_points = num_target
@@ -50,64 +69,33 @@ def gen(x_values, gp, batch_size, num_context, num_target, iterations, testing) 
         yield (context_x, context_y, target_x), target_y
 
 
-def get_gp_curve_generator(x_values: np.ndarray,
-                           gp: tfp.distributions.Distribution,
-                           num_context: int,
-                           num_target: int,
-                           iterations: int=10000,
-                           testing: bool=False) -> Callable:
-    
-    batch_size, num_total_points, _ = x_values.shape
+def get_gp_curve_generator_from_uniform(iterations: int=10000,
+                                        batch_size: int=64,
+                                        kernel_length_scale: float=0.4,
+                                        min_num_context: int=3,
+                                        max_num_context: int=10,
+                                        min_num_target: int=2,
+                                        testing: bool=False) -> Callable:
     
     return partial(gen,
-                   x_values=x_values,
-                   gp=gp,
                    batch_size=batch_size,
-                   num_context=num_context,
-                   num_target=num_target,
                    iterations=iterations,
+                   kernel_length_scale=kernel_length_scale,
+                   min_num_context=min_num_context,
+                   max_num_context=max_num_context,
+                   min_num_target=min_num_target,
                    testing=testing)
 
 
-def get_gp_curve_generator_from_uniform(iterations: int=10000,
-                                        batch_size: int=64,
-                                        max_num_context: int=10,
-                                        kernel_length_scale: float=0.4,
-                                        testing: bool=False) -> Callable:
-    num_context = tf.random.uniform(shape=[],
-                                minval=MIN_NUM_CONTEXT,
-                                maxval=max_num_context,
-                                dtype=tf.int32)
-
-    num_target = tf.random.uniform(shape=[],
-                                minval=MIN_NUM_TARGET,
-                                maxval=max_num_context,
-                                dtype=tf.int32)
-    
-    num_total_points = num_context + num_target
-    
-    x_values = tf.random.uniform(shape=(batch_size, num_total_points, 1),
-                                    minval=-2,
-                                    maxval=2)
-    
-    kernel = tfp.math.psd_kernels.ExponentiatedQuadratic(length_scale=kernel_length_scale)
-    
-    gp = tfd.GaussianProcess(kernel, index_points=x_values, jitter=1.0e-4)
-    
-    return get_gp_curve_generator(x_values=x_values,
-                                  gp=gp,
-                                  num_context=num_context,
-                                  num_target=num_target,
-                                  iterations=iterations,
-                                  testing=testing)
-
-
-def load_regression_data(iterations: int=250, batch_size: int=32) -> Tuple[tf.data.Dataset, tf.data.Dataset]:
+def load_regression_data(iterations: int=250, batch_size: int=32,
+                         min_num_context: int=3, max_num_context: int=10, min_num_target: int=2) -> Tuple[tf.data.Dataset, tf.data.Dataset]:
     train_ds = tf.data.Dataset.from_generator(
         get_gp_curve_generator_from_uniform(
             iterations=iterations,
             batch_size=batch_size,
-            max_num_context=10,
+            min_num_context=min_num_context,
+            max_num_context=max_num_context,
+            min_num_target=min_num_target,
             testing=False),
         output_types=((tf.float32, tf.float32, tf.float32), tf.float32)
     )
@@ -115,6 +103,9 @@ def load_regression_data(iterations: int=250, batch_size: int=32) -> Tuple[tf.da
         get_gp_curve_generator_from_uniform(
             iterations=iterations,
             batch_size=batch_size,
+            min_num_context=min_num_context,
+            max_num_context=max_num_context,
+            min_num_target=min_num_target,
             testing=True),
         output_types=((tf.float32, tf.float32, tf.float32), tf.float32)
     )
@@ -126,20 +117,26 @@ def load_regression_data(iterations: int=250, batch_size: int=32) -> Tuple[tf.da
 
 
 
-class DataGenerator():
+class RegressionDataGenerator():
     """Class that uses load_regression_data to create datasets.
     """
-    def __init__(self, iterations: int=250, batch_size: int=32):
+    def __init__(self, iterations: int=250, batch_size: int=32,
+                 min_num_context: int=3, max_num_context: int=10, min_num_target: int=2):
         self.iterations = iterations
         self.batch_size = batch_size
+        self.min_num_context = min_num_context
+        self.max_num_context = max_num_context
+        self.min_num_target = min_num_target
         
         self.train_ds, self.test_ds = load_regression_data(
-            iterations=iterations, batch_size=batch_size)
+            iterations=iterations, batch_size=batch_size,
+            min_num_context=min_num_context,
+            max_num_context=max_num_context,
+            min_num_target=min_num_target)
     
-    def plot_random_batch(self, figsize=(8, 5)):
-        """Plot a random batch from the train_ds.
-        """
-        (context_x, context_y, target_x), target_y = next(iter(self.train_ds.take(1)))
+    
+    @staticmethod
+    def plot_batch(context_x, context_y, target_x, target_y, figsize=(8, 5)):
         context_x = context_x.numpy()
         context_y = context_y.numpy()
         target_x = target_x.numpy()
@@ -150,4 +147,12 @@ class DataGenerator():
         ax.scatter(context_x[0, :, 0], context_y[0, :, 0], marker="x", c="red", label='Observations')
         ax.legend()
 
+        return fig, ax
+    
+    
+    def plot_random_batch(self, figsize=(8, 5)):
+        """Plot a random batch from the train_ds.
+        """
+        (context_x, context_y, target_x), target_y = next(iter(self.train_ds.take(1)))
+        fig, ax = RegressionDataGenerator.plot_batch(context_x, context_y, target_x, target_y, figsize=figsize)
         return fig, ax
