0549f6ee920466cf4b4d41cbef9b424a1de978f8
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2023-03-13 16:45:10 +0000

    Change figures for poster/report

--- dataloader/regression_data_generator_base.py
+++ dataloader/regression_data_generator_base.py
@@ -71,23 +71,28 @@ class RegressionDataGeneratorBase(ABC):
     
     
     @staticmethod
-    def plot_first_elt_of_batch(context_x, context_y, target_x, target_y, figsize=(8, 5)):
+    def plot_first_elt_of_batch(context_x, context_y, target_x, target_y,
+                                ax: Optional[plt.Axes]=None,
+                                figsize=(8, 5)):
         """Plot the first element of a batch."""
+        
+        if ax is None:
+            fig, ax = plt.subplots(figsize=figsize)
+        
         context_x = context_x.numpy()
         context_y = context_y.numpy()
         target_x = target_x.numpy()
         target_y = target_y.numpy()
         
-        fig, ax = plt.subplots(figsize=figsize)
         ax.scatter(target_x[0, :, 0], target_y[0, :, 0], c="blue", label='Target')
         ax.scatter(context_x[0, :, 0], context_y[0, :, 0], marker="x", c="red", label='Observations')
         ax.legend()
 
-        return fig, ax
+        return ax
     
     
     def plot_first_elt_of_random_batch(self, figsize=(8, 5)):
         """Plot a random batch from the training set."""
         (context_x, context_y, target_x), target_y = next(iter(self.train_ds.take(1)))
-        fig, ax = RegressionDataGeneratorBase.plot_first_elt_of_batch(context_x, context_y, target_x, target_y, figsize=figsize)
-        return fig, ax
+        ax = RegressionDataGeneratorBase.plot_first_elt_of_batch(context_x, context_y, target_x, target_y, figsize=figsize)
+        return ax
